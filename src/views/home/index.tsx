f9e4c363130907630a0388b310b925c5e08b71bf
Joshua Farayola <farayolajoshua@gmail.com>
2025-01-08 16:29:25 +0100

    generate random color for input field in HomeView component

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,6 +1,9 @@
+import { random } from "@ctrl/tinycolor";
 import { Form } from "react-router";
 
 export default function HomeView() {
+  const colour = random().toHexString();
+
   return (
     <Form method="post" action="/">
       <div>
@@ -9,7 +12,7 @@ export default function HomeView() {
       </div>
       <div>
         <label htmlFor="colour">Colour</label>
-        <input type="color" name="colour" id="colour" />
+        <input type="color" name="colour" id="colour" value={colour} readOnly />
       </div>
       <button type="submit">Login</button>
     </Form>
