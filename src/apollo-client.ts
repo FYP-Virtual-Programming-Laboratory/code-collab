c434509ff4110e6e70be6bed56c33a3e04f7ea41
Joshua Farayola <farayolajoshua@gmail.com>
2025-02-21 09:04:27 +0100

    feat: optimize Apollo Client instantiation by caching clients based on GraphQL URL

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,9 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
 import { KVStore } from "./lib/integration/configure";
 
+const clientMaps: Map<string, ApolloClient<NormalizedCacheObject>> = new Map();
+
 export function getApolloClient() {
-  return new ApolloClient({
-    uri: KVStore.gqlUrl,
-    cache: new InMemoryCache(),
-  });
+  const uri = KVStore.gqlUrl;
+
+  let client = clientMaps.get(uri);
+
+  if (!client) {
+    client = new ApolloClient({
+      uri,
+      cache: new InMemoryCache(),
+    });
+
+    clientMaps.set(uri, client);
+  }
+
+  return client;
 }
