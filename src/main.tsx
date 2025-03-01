12f765662ca452f301decf3093e936f9993cfaf9
Joshua Farayola <farayolajoshua@gmail.com>
2025-03-01 12:02:53 +0100

    feat: update user configuration to retrieve from URL parameters

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 
 import { CodeCollab, configure } from "./index";
 
+const user =
+  new URLSearchParams(window.location.search).get("user") || "farayolaj";
+console.log(user);
+
 configure({
   sessionId: "123456",
-  user: "farayolaj",
+  user,
 }).then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
